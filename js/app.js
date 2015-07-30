7d47a898ea5fcd84c449dbd8e25122bac14f177a
tuskerette <valeria.graffeo@gmail.com>
2015-07-30 17:59:06 -0400

    added rotate function

--- js/app.js
+++ js/app.js
@@ -41,3 +41,34 @@ interact('.draggable')
 
   // this is used later in the resizing demo
   window.dragMoveListener = dragMoveListener;
+
+
+// Rotate
+$(document).ready(function() {
+
+  function rotateOnMouse(e, pw) {
+      var offset = pw.offset();
+      var center_x = (offset.left) + ($(pw).width() / 2);
+      var center_y = (offset.top) + ($(pw).height() / 2);
+      var mouse_x = e.pageX;
+      var mouse_y = e.pageY;
+      var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+      var degree = (radians * (180 / Math.PI) * -1) + 100;
+      //            window.console.log("de="+degree+","+radians);
+      $(pw).css('-moz-transform', 'rotate(' + degree + 'deg)');
+      $(pw).css('-webkit-transform', 'rotate(' + degree + 'deg)');
+      $(pw).css('-o-transform', 'rotate(' + degree + 'deg)');
+      $(pw).css('-ms-transform', 'rotate(' + degree + 'deg)');
+  }
+
+  $('.drop div').mousedown(function(e) {
+    e.preventDefault(); // prevents the dragging of the image.
+    $(document).bind('mousemove.rotateImg', function(e2) {
+      rotateOnMouse(e2, $('.drop div'));
+    });
+  });
+
+  $(document).mouseup(function(e) {
+    $(document).unbind('mousemove.rotateImg');
+  });
+});
