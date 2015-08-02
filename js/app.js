483d4b89ec1b29059d68d1b20deb702ef0d0b6c0
tuskerette <valeria.graffeo@gmail.com>
2015-08-02 11:09:11 -0400

    added more icons to the screen

--- js/app.js
+++ js/app.js
@@ -3,44 +3,53 @@ interact('.draggable')
   .draggable({
     // enable inertial throwing
     inertia: true,
-    // keep the element within the area of it's parent
+    // keep the element within the area of its parent
     restrict: {
       restriction: "parent",
       endOnly: true,
       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
     },
-
-    // call this function on every dragmove event
     onmove: dragMoveListener,
-    // call this function on every dragend event
-    // onend: function (event) {
-    //   var textEl = event.target.querySelector('p');
-
-    //   // textEl && (textEl.textContent =
-    //   //   'moved a distance of '
-    //   //   + (Math.sqrt(event.dx * event.dx +
-    //   //                event.dy * event.dy)|0) + 'px');
-    // }
   });
 
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+function dragMoveListener (event) {
+  var target = event.target,
+      // keep the dragged position in the data-x/data-y attributes
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+  // translate the element
+  target.style.webkitTransform =
+  target.style.transform =
+    'translate(' + x + 'px, ' + y + 'px)';
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
+  // update the posiion attributes
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
 
-  // this is used later in the resizing demo
-  window.dragMoveListener = dragMoveListener;
+// this is used later in the resizing demo
+window.dragMoveListener = dragMoveListener;
+
+
+// Multi-touch rotation (interactjs)
+// var angle = 0;
+
+// interact('#rotate-area').gesturable({
+//   onmove: function (event) {
+//     var arrow = document.getElementById('search2');
+
+//     angle += event.da;
+
+//     arrow.style.webkitTransform =
+//     arrow.style.transform =
+//       'rotate(' + angle + 'deg)';
+
+//     // document.getElementById('angle-info').textContent =
+//     //   angle.toFixed(2) + '°';
+//   }
+// });
+
 
 
 // Rotate
@@ -54,7 +63,6 @@ $(document).ready(function() {
       var mouse_y = e.pageY;
       var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
       var degree = (radians * (180 / Math.PI) * -1) + 100;
-      //            window.console.log("de="+degree+","+radians);
       $(pw).css('-moz-transform', 'rotate(' + degree + 'deg)');
       $(pw).css('-webkit-transform', 'rotate(' + degree + 'deg)');
       $(pw).css('-o-transform', 'rotate(' + degree + 'deg)');
@@ -72,3 +80,5 @@ $(document).ready(function() {
     $(document).unbind('mousemove.rotateImg');
   });
 });
+
+
