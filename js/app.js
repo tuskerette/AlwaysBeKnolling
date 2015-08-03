e868c35d2afe5cd34d3003d4f4cb648c162f7bd6
tuskerette <valeria.graffeo@gmail.com>
2015-08-03 14:51:10 -0400

    rotate and drag working perfectly

--- js/app.js
+++ js/app.js
@@ -61,41 +61,8 @@ window.dragMoveListener = dragMoveListener;
 
 
 $('#rotateitem').on('click', function (event) {
-    $('#sunglasses2').toggleClass('rotate draggable');
+    $('.glyphicon').toggleClass('rotate draggable');
   });
 
-
-
-// OLD Rotate
-// $(document).ready(function() {
-
-  // function rotateOnMouse(e, pw) {
-  //     var offset = pw.offset();
-  //     console.log(offset);
-  //     var center_x = (offset.left) + ($(pw).width() / 2);
-  //     var center_y = (offset.top) + ($(pw).height() / 2);
-  //     var mouse_x = e.pageX;
-  //     var mouse_y = e.pageY;
-  //     var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
-  //     var degree = (radians * (180 / Math.PI) * -1) + 100;
-  //     $(pw).css('-moz-transform', 'rotate(' + degree + 'deg)');
-  //     $(pw).css('-webkit-transform', 'rotate(' + degree + 'deg)');
-  //     $(pw).css('-o-transform', 'rotate(' + degree + 'deg)');
-  //     $(pw).css('-ms-transform', 'rotate(' + degree + 'deg)');
-  // }
-
-  // $('.rotate').mousedown(function(e) {
-  //   e.preventDefault(); // prevents the dragging of the image.
-  //   $(document).bind('mousemove.rotateImg', function(e) {
-  //     rotateOnMouse(e, $('#sunglasses2'));
-  //   });
-  // });
-
-  // $(document).mouseup(function(e) {
-  //   $(document).unbind('mousemove.rotateImg');
-  // });
-
-
- // });
 
 
